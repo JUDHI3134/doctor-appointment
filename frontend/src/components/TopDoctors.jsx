c5078bc26ba1ef6a55f640a31cb0ddf63e35dba9
umesh604@gmail.com <umesh604@gmail.com>
2024-09-06 12:38:35 +0530

    add scroll-smooth and context

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import { doctors } from '../assets/assets'
+import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext';
 
 const TopDoctors = () => {
     const navigate = useNavigate();
+    const {doctors} = useContext(AppContext)
   return (
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
       <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
@@ -22,7 +23,7 @@ const TopDoctors = () => {
             </div>
         ))}
       </div>
-      <button className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>More</button>
+      <button onClick={()=>{navigate("/doctors"); scrollTo(0,0)}} className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>More</button>
     </div>
   )
 }
