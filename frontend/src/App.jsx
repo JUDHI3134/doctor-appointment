252b7e9d5b450a3f510a4264e5b90500f66d2556
umesh604@gmail.com <umesh604@gmail.com>
2024-09-06 10:32:16 +0530

    create all route for pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Doctors from './pages/Doctors'
+import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import MyProfile from './pages/MyProfile'
+import MyAppionetments from './pages/MyAppionetments'
+import Appointment from './pages/Appointment'
 
 const App = () => {
   return (
-    <div className='bg-green-500 p-4'>
-      Hello
+    <div className='mx-4 sm:mx-[10%]'>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/doctors' element={<Doctors/>} />
+        <Route path='/doctors/:speciality' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/my-profile' element={<MyProfile/>} />
+        <Route path='/my-appointments' element={<MyAppionetments/>} />
+        <Route path='/appointment/:docId' element={<Appointment/>} />
+      </Routes>
     </div>
   )
 }
