17c4b6f724bea5c144bfd97c08f0928f57283be7
umesh604@gmail.com <umesh604@gmail.com>
2024-09-06 19:50:57 +0530

    create appointment page ui

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -6,11 +6,11 @@ export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
      
-    
+    const currencySymbol = '$'
 
 
     const value = {
-        doctors
+        doctors,currencySymbol
     }
 
     return <AppContext.Provider value={value}>
