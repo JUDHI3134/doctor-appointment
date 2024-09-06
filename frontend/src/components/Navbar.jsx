e26c11672b9a1a46fd74628270f41cb3b3268cde
umesh604@gmail.com <umesh604@gmail.com>
2024-09-06 15:00:54 +0530

    create all doctors page ui

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     const [token,settoken] = useState(true)
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
-      <img className='w-44 cursor-pointer' src={assets.logo} alt="" />
+      <img onClick={()=>navigate('/')} className='w-44 cursor-pointer' src={assets.logo} alt="" />
       <ul className='hidden md:flex items-start gap-5 font-medium'>
         <NavLink to='/'>
             <li className='py-1'>HOME</li>
--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -9,7 +9,7 @@ const SpecialityMenu = () => {
       <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors, <br className='hidden sm:block'/> Schedule your appointment hassle-free</p>
       <div className='flex sm:justify-center gap-4 pt-5 w-full overflow-scroll'>
         {specialityData.map((item,index)=>(
-            <Link onClick={()=>scrollTo(0,0)} className='flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500' key={index} to={`/doctors/${item.speciality}`}>
+            <Link onClick={()=>scrollTo(0,0)} to={`/doctors/${item.speciality}`} className='flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500' key={index} >
                 <img className='w-16 sm:w-24 mb-2' src={item.image} alt="" />
                 <p>{item.speciality}</p>
             </Link>
