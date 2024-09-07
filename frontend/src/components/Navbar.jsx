8b6dac32581e31454f95ee97ce7ac80892d72f5c
umesh604@gmail.com <umesh604@gmail.com>
2024-09-07 10:15:49 +0530

    create mobile responsive

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { NavLink, useNavigate } from 'react-router-dom'
 const Navbar = () => {
     const navigate = useNavigate();
 
-    const [shoeMenu, setShowMenu]= useState(false)
+    const [showMenu, setShowMenu]= useState(false)
     const [token,settoken] = useState(true)
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
@@ -42,6 +42,26 @@ const Navbar = () => {
           </div>
         </div>    :
         <button onClick={()=>navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full hidden md:block'>Create account</button>}
+
+
+
+
+          <img onClick={()=>setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon} alt="" />
+          {/* ------------------Mobile menu------------ */}
+          <div className={` ${showMenu ? 'fixed w-full' : 'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
+            <div className='flex items-center justify-between px-5 py-6'>
+              <img className='w-36' src={assets.logo} alt="" />
+              <img className='w-7' onClick={()=>setShowMenu(false)} src={assets.cross_icon} alt="" />
+            </div>
+            <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
+              <NavLink onClick={()=>setShowMenu(false)} to="/"><p className="px-4 py-2 rounded inline-block">HOME</p></NavLink>
+              <NavLink onClick={()=>setShowMenu(false)} to="/doctors"><p className="px-4 py-2 rounded inline-block">ALL DOCTORS</p></NavLink>
+              <NavLink onClick={()=>setShowMenu(false)} to="/about"><p className="px-4 py-2 rounded inline-block">ABOUT</p></NavLink>
+              <NavLink onClick={()=>setShowMenu(false)} to="/contact"><p className="px-4 py-2 rounded inline-block">CONTACT</p></NavLink>
+            </ul>
+          </div>
+
+
       </div>
     </div>
   )
--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -7,6 +7,7 @@ const Doctors = () => {
 
     const {doctors} = useContext(AppContext)
     const [filterDoc,setFilterDoc] = useState([])
+    const [showFilter, setShowFilter] = useState(false)
     const navigate = useNavigate();
 
     const applyFilter = () =>{
@@ -25,7 +26,8 @@ const Doctors = () => {
     <div>
       <p className='text-gray-600'>Browsw through the doctor specialist</p>
       <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
-        <div className='flex flex-col gap-4 text-sm text-gray-600'>
+      <button className={`px-3 py-1 border rounded text-sm transition-all sm:hidden ${showFilter ? 'bg-primary text-white' : ''}`} onClick={()=>setShowFilter(prev => !prev)}>Filters</button>
+        <div className={` flex-col gap-4 text-sm text-gray-600 ${showFilter ? "flex" : "hidden sm:flex"}`}>
             <p onClick={()=>speciality === "General physician" ? navigate('/doctors') : navigate('/doctors/General physician')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-default ${speciality === "General physician" ? "bg-indigo-100 text-black" : ""}`}>General physician</p>
             <p onClick={()=>speciality === "Gynecologist" ? navigate('/doctors') : navigate('/doctors/Gynecologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-default ${speciality === "Gynecologist" ? "bg-indigo-100 text-black" : ""}`}>Gynecologist</p>
             <p onClick={()=>speciality === "Dermatologist" ? navigate('/doctors') : navigate('/doctors/Dermatologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-default ${speciality === "Dermatologist" ? "bg-indigo-100 text-black" : ""}`}>Dermatologist</p>
